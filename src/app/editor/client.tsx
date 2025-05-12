472b9bd19def76519bf4b650eccf3a04a1af865a
Azharuddin Mulla <azharuddin.mulla@amla.io>
2025-05-12 14:08:20 +0530

    Initial Changes

---
+++ src/app/editor/client.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Config, Puck } from "@measured/puck";
+import "@measured/puck/puck.css";
+import "./style.css";
+
+const config: Config<{
+  HeadingBlock: {
+    children: string;
+  };
+}> = {
+  components: {
+    HeadingBlock: {
+      fields: {
+        children: {
+          type: "text",
+        },
+      },
+      defaultProps: {
+        children: "TExt",
+      },
+      render: ({ children }) => {
+        return (
+          <div>
+            {children}
+            <ProfileCard />
+            <div className="test">helo</div>
+          </div>
+        );
+      },
+    },
+  },
+};
+
+const initialData = {};
+
+export default function Client() {
+  return <Puck config={config} data={initialData} />;
+}
+
+const ProfileCard = () => {
+  return (
+    <div className="profile-card">
+      <img src="profile.jpg" alt="Profile Picture" className="profile-img" />
+      <h2>Jane Doe</h2>
+      <p className="title">Software Developer</p>
+      <p>San Francisco, CA</p>
+      <a href="mailto:jane@example.com">Contact</a>
+    </div>
+  );
+};
---
+++ src/app/editor/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Client from "./client";
+
+function page() {
+  return (
+    <div>
+      <Client />
+    </div>
+  );
+}
+
+export default page;
